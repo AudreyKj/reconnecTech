d8e1314c137664474a79ba62af61c568283c6a4c
Mansi Patel <45537538+mansijp@users.noreply.github.com>
2022-06-12 14:57:31 -0400

    About Us Page

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -4,9 +4,29 @@ import '../App.css';
 function About() {
   return (
     <div>
-        <h1>About Us</h1>
-        <h3></h3>
+      <div id="about-wrapper">
+          <h1 id="title">About Us</h1>
+          <p id="message">We are a women-owned business located in the heart of London's<br></br>
+          Industrial Park district. We are on a mission to help new residents<br></br> 
+          recently displaced due to dire world events and the low to no<br></br> 
+          earners living here in the United Kingdom. <br></br><br></br>
+
+          With the generous donations from local businesses and individuals,<br></br> 
+          we are determined to close the existing technology gap. By offering<br></br>
+          refurbished devices such as smartphones, tablets, and laptops, we<br></br> 
+          are simultaneously helping businesses and individuals reduce<br></br> 
+          electronic waste and elevate those in need. With their new-used<br></br> 
+          devices, individuals can readily search for new opportunities both<br></br> 
+          in work and in furthering their education, thus improving their <br></br> 
+          livelihood.</p>
+
+          
+      </div>
+      
+      <div id="footer"></div>
+      
     </div>
+
   );
 }
 
